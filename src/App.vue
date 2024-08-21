d2d243b6ff8865c62a62c9c84fa461c3a9762759
董笑天 <dongxiaotian@talkweb.com.cn>
2024-08-21 18:42:17 +0800

    前端转发工程初始化

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,6 @@
 <template>
   <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
-    <router-view/>
+    <forwarding />
   </div>
 </template>
 
@@ -30,3 +26,6 @@ nav {
   }
 }
 </style>
+<script setup>
+import Forwarding from "@/views/forwarding.vue";
+</script>
---
+++ src/views/forwarding.vue
@@ -0,0 +1,31 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  name: "forwarding",
+  methods: {
+    // 处理转发
+    handleForwarding(){
+      const queryParams = this.$route.query
+      // 判断是否存在登录参数
+      if(!queryParams.loginParams) return
+      const loginKey = queryParams.loginParams
+      if(!queryParams[loginKey]) return
+      const loginValue = queryParams[loginKey]
+      // 不存在目标地址
+      if(!queryParams.path) return
+      localStorage.setItem(queryParams.loginParams, loginValue)
+      window.location.href = decodeURIComponent(queryParams.path)
+    }
+  },
+  mounted() {
+    this.handleForwarding()
+  }
+}
+</script>
+
+<style scoped lang="less">
+
+</style>
