01148c1a336b6a1396056e64bd97f8afe1e78939
董笑天 <dongxiaotian@talkweb.com.cn>
2024-08-22 16:15:13 +0800

    优化

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,3 +1,11 @@
 const CryptoJS = require('crypto-js')
 
 const key = CryptoJS.enc.Utf8.parse('dify_ai_0525')
+
+export function decryptAES(word) {
+    const decrypt = CryptoJS.AES.decrypt(word, key, {
+        mode: CryptoJS.mode.ECB,
+        padding:  CryptoJS.pad.Pkcs7
+    })
+    return CryptoJS.enc.Utf8.stringify(decrypt).toString()
+}
--- src/views/forwarding.vue
+++ src/views/forwarding.vue
@@ -5,6 +5,13 @@
 <script>
 export default {
   name: "forwarding",
+  data(){
+    return {
+      difyLoginKey: 'console_token',
+      difyConversationIdInfo: 'conversationIdInfo',
+      difyToken: 'token'
+    }
+  },
   methods: {
     // 处理转发
     handleForwarding(){
@@ -16,7 +23,15 @@ export default {
       const loginValue = queryParams[loginKey]
       // 不存在目标地址
       if(!queryParams.path) return
-      localStorage.setItem(queryParams.loginParams, loginValue)
+      // 判断是否存在应用会话选中记录
+      if(queryParams[this.difyConversationIdInfo]){
+        localStorage.setItem(this.difyConversationIdInfo, decodeURI(queryParams[this.difyConversationIdInfo]))
+      }
+      // 判断是否存在应用会话对象
+      if(queryParams[this.difyToken]){
+        localStorage.setItem(this.difyToken, decodeURI(queryParams[this.difyToken]))
+      }
+      localStorage.setItem(this.difyLoginKey, loginValue)
       window.location.href = decodeURIComponent(queryParams.path)
     }
   },
